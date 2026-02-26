99af30e25807cac3b63687d0016037d71a081946
ddtrvlr <info.wearedood@gmail.com>
2026-02-26 11:47:52 +0100

    fix(cli): correct help text for faucet command
    Removed help text for encrypting a backup phrase and replaced it with a description for requesting Testnet STX tokens.

--- packages/cli/src/argparse.ts
+++ packages/cli/src/argparse.ts
@@ -651,18 +651,7 @@ export const CLI_ARGS = {
       minItems: 1,
       maxItems: 1,
       help:
-        'Encrypt a 12-word backup phrase, which can be decrypted later with the ' +
-        '`decrypt_backup_phrase` command.  The password will be prompted if not given.\n' +
-        '\n' +
-        'Example:\n' +
-        '\n' +
-        '```console\n' +
-        '     $ # password is "asdf"\n' +
-        '     $ blockstack-cli encrypt_keychain "section amount spend resemble spray verify night immune tattoo best emotion parrot"\n' +
-        '     Enter password:\n' +
-        '     Enter password again:\n' +
-        '     M+DnBHYb1fgw4N3oZ+5uTEAua5bAWkgTW/SjmmBhGGbJtjOtqVV+RrLJEJOgT35hBon4WKdGWye2vTdgqDo7+HIobwJwkQtN2YF9g3zPsKk=' +
-        '```\n',
+        'Request Testnet STX tokens for a given Stacks address.',
       group: 'Key Management',
     },
     gaia_dump_bucket: {
